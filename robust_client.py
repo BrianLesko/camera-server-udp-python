d78e82e70a8f1a6a2acc84601db27d073f942fd7
Brian Lesko <brianjosephlesko@gmail.com>
2024-04-04 18:40:15 -0400

    Create robust_client.py

---
+++ robust_client.py
@@ -0,0 +1,12 @@
+# Brian lesko 
+
+import socket
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client_address = ('172.20.10.2', 8000)  # Replace with the client's IP address and port
+client.bind(client_address)
+print('Client is listening at', client_address)
+while True: 
+    chunk, addr = client.recvfrom(1000)
+    print(chunk.decode('utf-8'))
+
+
