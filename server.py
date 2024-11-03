97e3a6f700e6b50e63b09d64bfe9983070bdf39b
Brian Lesko <brianjosephlesko@gmail.com>
2024-11-03 13:43:34 -0500

    Update server.py

--- server.py
+++ server.py
@@ -3,12 +3,11 @@ import cv2  # pip install opencv-python-headless
 import socket
 import argparse
 
-camera = cv2.VideoCapture(0)  # on a mac you can use either your mac webcam or an iphone camera using continuity camera! for me, my iphone was (1) and my mac webcam was (0)
-# Limit the size and FPS to increase speed
-camera.set(cv2.CAP_PROP_FPS, 60)  # FPS
+camera = cv2.VideoCapture(0)
+camera.set(cv2.CAP_PROP_FPS, 60)
 camera.set(cv2.CAP_PROP_FRAME_WIDTH, 720)
 camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 600)
-camera.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))  # compression method
+camera.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
 
 def get_frame(): 
     global camera
@@ -20,22 +19,33 @@ def get_frame():
     except:
         return np.zeros((300, 300, 3))
 
+def get_host_ip():
+    """Retrieve the local IP address of the host by connecting to an external server"""
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        try:
+            # Doesn't actually connect to google.com, just retrieves local IP used for outbound connection
+            s.connect(("8.8.8.8", 80))
+            host_ip = s.getsockname()[0]
+        except Exception as e:
+            print("Error retrieving host IP:", e)
+            host_ip = "127.0.0.1"  # Fallback to localhost
+    return host_ip
+
 def main(client_ip):
     # UDP socket
     server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-    host_name = socket.gethostname()
-    host_ip = socket.gethostbyname(host_name)
-    print('Host:', host_ip)
+    host_ip = get_host_ip()
+    print('Host IP:', host_ip)
     server.bind((host_ip, 8000))  # Bind the socket to a specific address
 
-    client_address = (client_ip, 8000)  # Use the passed client IP address
+    client_address = (client_ip, 8000)
 
     while True:
         frame = get_frame()
         data = cv2.imencode('.jpg', frame)[1].tobytes()
 
         # Split the data into chunks
-        chunk_size = 500  # Maximum UDP packet size
+        chunk_size = 500
         for i in range(0, len(data), chunk_size):
             chunk = data[i:i+chunk_size]
             server.sendto(chunk, client_address)
